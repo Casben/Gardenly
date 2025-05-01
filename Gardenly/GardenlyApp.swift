800f457b8a9e9e91aaeee43c16d60b678e98c4d5
casben <herbiedodge@gmail.com>
2025-05-01 10:41:33 -0500

    initial commit

---
+++ Gardenly/GardenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenlyApp.swift
+//  Gardenly
+//
+//  Created by Herbert Dodge on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GardenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

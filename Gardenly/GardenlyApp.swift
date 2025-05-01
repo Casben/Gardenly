9d0323d37fc77c961107bf456ed97ad05aaec0aa
casben <herbiedodge@gmail.com>
2025-05-01 12:27:12 -0500

    add VegetableTabBarScreen and navigation stack for the app's main flow

--- Gardenly/GardenlyApp.swift
+++ Gardenly/GardenlyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GardenlyApp: App {
     var body: some Scene {
         WindowGroup {
-            VegetableListScreen()
+            VegetableTabBarScreen()
         }
     }
 }
---
+++ Gardenly/Screens/VegetableTabBarScreen.swift
@@ -0,0 +1,42 @@
+//
+//  VegetableTabBarScreen.swift
+//  Gardenly
+//
+//  Created by Herbert Dodge on 5/1/25.
+//
+
+import SwiftUI
+
+struct VegetableTabBarScreen: View {
+    var body: some View {
+        TabView {
+            NavigationStack {
+                VegetableListScreen()
+            }
+            .tabItem {
+                Image(systemName: "leaf")
+                Text("Vegetables")
+            }
+            
+            NavigationStack {
+                Text("MyGardenScreen")
+            }
+            .tabItem {
+                Image(systemName: "house")
+                Text("My Garden")
+            }
+            
+            NavigationStack {
+                Text("Pests")
+            }
+            .tabItem {
+                Image(systemName: "ladybug")
+                Text("Pests")
+            }
+        }
+    }
+}
+
+#Preview {
+    VegetableTabBarScreen()
+}

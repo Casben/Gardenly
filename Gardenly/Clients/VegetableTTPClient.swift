a1f03e423561017dce2cf64a88eec06ab7b1255b
casben <herbiedodge@gmail.com>
2025-05-01 11:12:21 -0500

    create VegetableTTPClient

---
+++ Gardenly/Clients/VegetableTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//  VegetableTTPClient.swift
+//  Gardenly
+//
+//  Created by Herbert Dodge on 5/1/25.
+//
+
+import Foundation
+
+struct VegetableTTPClient {
+    
+//    func fetchVegetable() -> [Vegetable] {
+//        
+//    }
+}

a328c678a6049f6d40e3abe6b0e092fb7ba419f6
casben <herbiedodge@gmail.com>
2025-05-02 09:08:31 -0500

    add MyGardenVegetable to context container

--- Gardenly/GardenlyApp.swift
+++ Gardenly/GardenlyApp.swift
@@ -12,7 +12,7 @@ struct GardenlyApp: App {
     var body: some Scene {
         WindowGroup {
             VegetableTabBarScreen()
-                .modelContainer(<#T##container: ModelContainer##ModelContainer#>)
+                .modelContainer(for: [Vegetable.self, MyGardenVegetable.self])
         }
     }
 }

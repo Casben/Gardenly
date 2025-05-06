1cdc6410e889cce98c4fd1fc900929adc45527ab
casben <herbiedodge@gmail.com>
2025-05-06 09:36:04 -0500

    add ui to start saving photos to notes

--- Gardenly/Screens/AddNoteScreen.swift
+++ Gardenly/Screens/AddNoteScreen.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct AddNoteScreen: View {
     let myGardenVegetable: MyGardenVegetable
     
     @State private var noteTitle = ""
     @State private var noteBody = ""
+    @State private var selectedPhotoItem: PhotosPickerItem? = nil
+    @State private var uiImage: UIImage?
+    @State private var imageData: Data?
     
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var context
@@ -22,6 +26,41 @@ struct AddNoteScreen: View {
             TextField("Title", text: $noteTitle)
             TextEditor(text: $noteBody)
                 .frame(minHeight: 200)
+            
+            HStack(spacing: 20) {
+                PhotosPicker(selection: $selectedPhotoItem, matching: .images, photoLibrary: .shared()) {
+                    ZStack {
+                        Circle()
+                            .fill(Color.blue.opacity(0.2))
+                            .frame(width: 60, height: 60)
+                        Image(systemName: "photo.on.rectangle")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+            .buttonStyle(.borderless)
+            
+            if let uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 5)
+                    .padding()
+            }
+        }
+        .task(id: selectedPhotoItem) {
+            if let selectedPhotoItem {
+                do {
+                    if let data = try await selectedPhotoItem.loadTransferable(type: Data.self) {
+                        uiImage = UIImage(data: data)
+                        imageData = data
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
         .toolbar {
             

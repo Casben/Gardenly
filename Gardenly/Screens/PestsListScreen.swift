593a2eb6981bf7d3aaf89e3c23027496ad5616f2
casben <herbiedodge@gmail.com>
2025-05-10 11:25:41 -0500

    add search functionality to PestListScreen

--- Gardenly/Screens/PestsListScreen.swift
+++ Gardenly/Screens/PestsListScreen.swift
@@ -10,14 +10,24 @@ import SwiftUI
 struct PestsListScreen: View {
     let pests: [Pest]
     
+    @State private var search: String = ""
+    
+    private var filteredPests: [Pest] {
+        if search.isEmptyOrWhiteSpace {
+            return pests
+        } else {
+            return pests.filter { $0.name.localizedCaseInsensitiveContains(search) }
+        }
+    }
     var body: some View {
-        List(pests) { pest in
+        List(filteredPests) { pest in
             NavigationLink {
                 PestDetailScreen(pest: pest)
             } label: {
                 PestCellView(pest: pest)
             }
         }
+        .searchable(text: $search)
         .listStyle(.plain)
         .navigationTitle("Pests")
     }
